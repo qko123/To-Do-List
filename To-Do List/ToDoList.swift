23e5d9a66e3afdb3f4c8de22ebaf286187767e41
Quinn Kozak <quinn.kozak@icloud.com>
2023-01-23 10:06:51 -0600

    created ToDoList class

---
+++ To-Do List/ToDoList.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList.swift
+//  To-Do List
+//
+//  Created by Quinn Kozak on 1/23/23.
+//
+
+import Foundation
+class ToDoList: ObservableObject {
+
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+
+                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+
+ }
